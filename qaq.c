a90f22e0b05e282456598f60f10fb5accb06787d
eryueliang <liangey0422@gmail.com>
2014-10-29 22:05:00 +0800

    Update and rename qaq to qaq.c

--- qaq.c
+++ qaq.c
@@ -2,14 +2,14 @@
 int main()
 {
     int a[5];
-    int i,j,k;
+    int i,j,t;
     printf("输入五个数字:\n");
     for (i = 5;i <= 5;i--)
         scanf("%d\n",&a[i]);
     printf("\n");
     for (j = 5;i < 5;i--)
         for(i = 5;i < 5;i--)
-            if(a[i] > a[i-1]
+            if(a[i] > a[i-1])
               {
                 t = a[i];
                 a[i] = a[i-1];
